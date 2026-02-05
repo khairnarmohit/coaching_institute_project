36168d7d69e98735e8e59d5e51eefe7d9c57d0e6
sanjivani <nakadesanjivani13@gmail.com>
2026-02-05 13:23:06 +0530

    changes courses and admission

--- routes/user.js
+++ routes/user.js
@@ -213,6 +213,7 @@ router.get("/course-details/:id", async (req, res) => {
 
     const sql = "SELECT * FROM courses_list WHERE id = ? AND status = 1";
     const data = await exe(sql, [id]);
+     const result = await exe("SELECT * FROM contact_details WHERE id = 1");
 
     if (data.length === 0) {
       return res.redirect("/courses");
@@ -226,7 +227,7 @@ router.get("/course-details/:id", async (req, res) => {
       course.features = [];
     }
 
-    res.render("user/course-details.ejs", { course });
+    res.render("user/course-details.ejs", { course ,contact: result[0] });
 
   } catch (err) {
     console.log(err);
